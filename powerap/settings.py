bfe434efc8a50773c3e48f334cda524356a1f3cd
nweze <dominicnweze16@gmail.com>
2021-10-14 16:28:06 +0100

    deploy static

--- powerap/settings.py
+++ powerap/settings.py
@@ -139,9 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'itelapp/static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
